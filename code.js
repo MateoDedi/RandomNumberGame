d498c307bf7c1a8be6d6837540c07864ef47ee6f
MateoDedi <mateod@live.com>
2023-09-20 11:09:20 +0200

    fist commit

---
+++ code.js
@@ -0,0 +1,28 @@
+let maxNum = parseInt(prompt("Please enter a maximum number:"));
+
+
+if ( !isNaN(maxNum)) {
+    alert("A L R I G H T !!")
+} else {
+    alert("You should enter a number")
+}
+
+let goal = Math.floor(Math.random() * maxNum) + 1;
+
+let count = 0;
+let guess = 0;
+
+do {
+    guess = parseInt(prompt(`Guess the number between 1 and ${maxNum} `))
+    if (guess == goal) {
+        alert(`AMAZING!! IT ONLY TOOK YOU ${count} TRY ♡(ﾐ ᵕ̣̣̣̣̣̣ ﻌ ᵕ̣̣̣̣̣̣ ﾐ)ﾉ`);
+    } else if (guess < goal) {
+        alert(`GUESS H I G H E R ! ! ୧(๑•̀ᗝ•́)૭`)
+        count++;
+    } else if (guess > goal) {
+        alert(`GUESS L O W E R ! ! ୧༼ಠ益ಠ༽୨`)
+        count++;
+    }  else {
+        alert("THIS ISN'T A NUMBER ! ! (ノಠ益ಠ)ノ彡┻━┻")
+    }
+} while (guess != goal);
